f659eeac4dae93b440f2e5affd111031905903ed
jbculbertson <jbculbertson@gmail.com>
2017-08-12 15:49:07 -0400

    Basic map functionality up!
    - NPM install --save google maps, which adds it to package.json
    - added .env to gitignore, and stored the google maps key there
    - basic map load functionality into index.js
    = added div id="map" container to index.html, and height styling to css

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,25 @@
 'use strict'
 
+const env = require('../../.env.js')
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const GoogleMapsLoader = require('google-maps')
+
+GoogleMapsLoader.KEY = env.GOOGLE_MAPS_API_KEY
 
 $(() => {
   setAPIOrigin(location, config)
+  // GoogleMapsLoader.onLoad(function(google) {
+  //   console.log('I just loaded google maps api')
+  // })
+
+  GoogleMapsLoader.load(function(google) {
+    console.log('I just fired within initMap')
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: {lat: 42.2495, lng: -71.0662}
+    })
+  })
 })
 
 // use require with a reference to bundle the file and use it in this file
