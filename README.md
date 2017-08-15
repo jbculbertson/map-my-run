6ef65d3a918bb3f117b47840072595c6f11b6cf6
jbculbertson <jbculbertson@gmail.com>
2017-08-15 08:52:36 -0400

    Add geolocation ability

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # Map My Run
-
-
-1.  Add a listener to the map, once it fires, a click creates a Marker
-map.addListener('click', function(event) {
-  placeMarkerAndPanTo(event.latLng, map)
-  })
-}
-<!-- NEED TO FIGURE OUT HOW TO REMOVE LISTENER -->
-
-2. add each marker to an array
-3. send that array to new Polyline
-4. 
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,27 +8,33 @@ const GoogleMapsLoader = require('google-maps')
 
 GoogleMapsLoader.KEY = env.GOOGLE_MAPS_API_KEY
 
-// GoogleMapsLoader.LIBRARIES = ['geometry', 'places']
+GoogleMapsLoader.LIBRARIES = ['geometry', 'places']
 $(() => {
   setAPIOrigin(location, config)
 
+  let map
   GoogleMapsLoader.load(function (google) {
+    // basic map initializer
     let milton = new google.maps.LatLng(42.2495, -71.0662)
     let boston = new google.maps.LatLng(42.3530, -71.0574)
     let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 11,
-      center: boston
+      zoom: 13,
+      center: milton
     })
     let poly = new google.maps.Polyline({
       strokeColor: '#000000',
       strokeOpacity: 1.0,
       strokeWeight: 3
     })
+    let infoWindow = new google.maps.InfoWindow()
     poly.setMap(map)
     map.addListener('click', addLatLng)
 
+    // add LatLng, and draw map on each click
     function addLatLng (event) {
       let path = poly.getPath()
+      let polyLengthinMiles = ((google.maps.geometry.spherical.computeLength(path)) * 0.000621371).toFixed(2)
+      $('#length').text(polyLengthinMiles + ' miles')
       path.push(event.latLng)
       let marker = new google.maps.Marker({
         position: event.latLng,
@@ -36,7 +42,32 @@ $(() => {
         map: map
       })
     }
+    // geolocate user
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        infoWindow.setPosition(pos)
+        infoWindow.setContent('Location found.')
+        infoWindow.open(map)
+        map.setCenter(pos)
+      }, function () {
+        handleLocationError(true, infoWindow, map.getCenter())
+      })
+    } else {
+  // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter())
+    }
   })
+  function handleLocationError (browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos)
+    infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.')
+    infoWindow.open(map)
+  }
 })
 
 //
