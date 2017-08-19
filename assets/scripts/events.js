4d45b23aafa93f8fe92ef88f369f16c6d240bb74
jbculbertson <jbculbertson@gmail.com>
2017-08-19 14:07:46 -0400

    Added remove marker
    - also update length on remove

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,1 +1,78 @@
 'use strict'
+
+const env = require('../../.env.js')
+const GoogleMapsLoader = require('google-maps')
+
+GoogleMapsLoader.KEY = env.GOOGLE_MAPS_API_KEY
+GoogleMapsLoader.LIBRARIES = ['geometry', 'places']
+
+let map
+let poly
+let google
+let markers = []
+
+const getCurrentLocation = function () {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    initialize(pos)
+  })
+}
+
+const initialize = function (pos) {
+  GoogleMapsLoader.load(function (google) {
+    const loc = {
+      lat: pos.lat,
+      lng: pos.lng
+    }
+    const mapOptions = {
+      zoom: 15,
+      center: loc,
+      mapTypeId: 'terrain'
+    }
+    let map = new google.maps.Map(document.getElementById('map'),
+    mapOptions)
+    let polyline = new google.maps.Polyline({
+      strokeColor: 'black',
+      strokeWeight: 3,
+      map: map
+    })
+    google.maps.event.addListener(map, 'click', function (event) {
+      addPoint(event.latLng)
+    })
+    function removePoint (marker) {
+      for (let i = 0; i < markers.length; i++) {
+        if (markers[i] === marker) {
+          markers[i].setMap(null)
+          markers.splice(i, 1)
+          polyline.getPath().removeAt(i)
+        }
+      }
+      const length = google.maps.geometry.spherical.computeLength(polyline.getPath())
+      const mileLength = (length * 0.000621371).toFixed(2)
+      $('#length').text('This run is ' + mileLength + ' miles long.')
+    }
+    function addPoint (latlng) {
+      const marker = new google.maps.Marker({
+        position: latlng,
+        animation: google.maps.Animation.DROP,
+        map: map
+      })
+      markers.push(marker)
+      polyline.getPath().setAt(markers.length - 1, latlng)
+      google.maps.event.addListener(marker, 'click', function (event) {
+        removePoint(marker)
+      })
+      const length = google.maps.geometry.spherical.computeLength(polyline.getPath())
+      const mileLength = (length * 0.000621371).toFixed(2)
+      $('#length').text('This run is ' + mileLength + ' miles long.')
+    }
+  })
+}
+
+module.exports = {
+  getCurrentLocation,
+  initialize
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,135 +1,13 @@
 'use strict'
 
-const env = require('../../.env.js')
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const mapEvents = require('./events')
 
-const GoogleMapsLoader = require('google-maps')
-
-GoogleMapsLoader.KEY = env.GOOGLE_MAPS_API_KEY
-
-GoogleMapsLoader.LIBRARIES = ['geometry', 'places']
 $(() => {
   setAPIOrigin(location, config)
-
-  let map
-  GoogleMapsLoader.load(function (google) {
-    // basic map initializer
-    let milton = new google.maps.LatLng(42.2495, -71.0662)
-    let boston = new google.maps.LatLng(42.3530, -71.0574)
-    let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: milton
-    })
-    let poly = new google.maps.Polyline({
-      strokeColor: '#000000',
-      strokeOpacity: 1.0,
-      strokeWeight: 3
-    })
-    let infoWindow = new google.maps.InfoWindow()
-    poly.setMap(map)
-    map.addListener('click', addLatLng)
-
-    // add LatLng, and draw map on each click
-    function addLatLng (event) {
-      let path = poly.getPath()
-      let polyLengthinMiles = ((google.maps.geometry.spherical.computeLength(path)) * 0.000621371).toFixed(2)
-      $('#length').text(polyLengthinMiles + ' miles')
-      path.push(event.latLng)
-      let marker = new google.maps.Marker({
-        position: event.latLng,
-        title: '#' + path.getLength(),
-        map: map
-      })
-    }
-    // geolocate user
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        let pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
-        infoWindow.setPosition(pos)
-        infoWindow.setContent('Location found.')
-        infoWindow.open(map)
-        map.setCenter(pos)
-      }, function () {
-        handleLocationError(true, infoWindow, map.getCenter())
-      })
-    } else {
-  // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter())
-    }
-  })
-  function handleLocationError (browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos)
-    infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.')
-    infoWindow.open(map)
-  }
+  $('#find').on('click', mapEvents.getCurrentLocation)
 })
-
-//
-// let gaBostonMarker = new google.maps.Marker({
-//   position: boston,
-//   map: map
-// })
-// let miltonMarker = new google.maps.Marker({
-//   position: milton,
-//   map: map
-// })
-//
-// let directionsService = new google.maps.DirectionsService()
-// let directionsDisplay = new google.maps.DirectionsRenderer()
-//
-// calculateAndDisplayRoute(directionsService, directionsDisplay)
-//
-// function calculateAndDisplayRoute (directionsService, directionsDisplay) {
-//   directionsService.route({
-//     origin: {lat: 42.3530, lng: -71.0574},
-//     destination: {lat: 42.2495, lng: -71.0662},
-//     travelMode: 'DRIVING'
-//   }, function (response, status) {
-//     if (status === 'OK') {
-//       directionsDisplay.setDirections(response)
-//       directionsDisplay.setMap(map)
-//     } else {
-//       console.log(status)
-//     }
-//   })
-// }
-
-// })
-//
-// function initMap() {
-//   let milton = {lat: 42.243, lng: -71.064}
-//   let map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 14,
-//     center: milton
-//    })
-//   let poly = new google.maps.Polyline({
-//    strokeColor: '#000000',
-//    strokeOpacity: 1.0,
-//    strokeWeight: 3
-//   })
-//   poly.setMap(map)
-//   map.addListener('click', addLatLng)
-//
-// function addLatLng(event) {
-//   let path = poly.getPath()
-//   path.push(event.latLng)
-//   let marker = new google.maps.Marker({
-//     position: event.latLng,
-//     title: '#' + path.getLength(),
-//     map: map
-//   })
-// }
-//
-// }
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
 require('./example')
