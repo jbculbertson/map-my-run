394ca3054b42e220bfa2784fa1f8eefa63c9edd6
jbculbertson <jbculbertson@gmail.com>
2017-08-22 20:11:01 -0400

    Add UI messaging around auth events

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -7,27 +7,43 @@ const signUpSuccess = (data) => {
 }
 
 const signUpFailure = (error) => {
+  $('.sign-up-modal-header').text('We may already have an account with that email')
+  $('.sign-up').val('')
+  $('.sign-in').val('')
   console.error('signup error is', error)
 }
 
 const signInSuccess = (data) => {
   store.user = data.user
+  $('.sign-up').val('')
+  $('.sign-in').val('')
+  $('#sign-in-modal').modal('hide')
+  $('#sign-up-modal').modal('hide')
+  $('.sign-up-modal-header').text('')
+  $('.sign-in-modal-header').text('')
   console.log('successful sign in')
 }
 
 const signInFailure = (error) => {
+  $('.sign-in-modal-header').text('Please make sure you have the correct credentials')
+  $('.sign-in').val('')
+  $('.sign-up').val('')
   console.error('signin error is', error)
 }
 
 const changePasswordSuccess = () => {
+  $('.change-password').val('')
+  $('.change-password-modal-header').text('Successfully changed password.')
   console.log('successfully changed password')
 }
 
 const changePasswordFailure = (error) => {
+  $('.change-password-modal-header').text('Incorrect credentials.')
   console.error(error)
 }
 
 const signOutSuccess = () => {
+  $('.change-password-modal-header').text('')
   console.log('successful sign out')
 }
 
