fd21d578618d62296342af3bd52757029201c6f7
jbculbertson <jbculbertson@gmail.com>
2017-08-23 08:49:38 -0400

    Add feature to initialize map on ShowOneRun
    Incomplete feature.
    
    Trying to rebuild a map2 object within ui.js, so that when I get a
    successful SHOW, I can rebuild a map to show that route.
    
    As of right now, its initializing with data.run.route[0] as the starting
    point, but google doesn't like it as a string.  Need to figure out a way
    to turn it into a number.

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -2,6 +2,35 @@
 const showAllRunsTemplate = require('./templates/show-all-runs.handlebars')
 const store = require('./store')
 
+const env = require('../../.env.js')
+const GoogleMapsLoader = require('google-maps')
+
+GoogleMapsLoader.KEY = env.GOOGLE_MAPS_API_KEY
+GoogleMapsLoader.LIBRARIES = ['geometry', 'places']
+
+const initialize = function (pos) {
+  console.log('fires within initialize2')
+  GoogleMapsLoader.load(function (google) {
+    console.log('fires within GoogleMapsLoader2')
+    const loc = {
+      lat: pos.lat,
+      lng: pos.lng
+    }
+    const mapOptions = {
+      zoom: 15,
+      center: loc,
+      mapTypeId: 'terrain'
+    }
+    const map = new google.maps.Map(document.getElementById('map2'),
+    mapOptions)
+    // polyline = new google.maps.Polyline({
+    //   strokeColor: 'black',
+    //   strokeWeight: 3,
+    //   map: map
+    // })
+  })
+}
+
 const createRunSuccess = (data) => {
   $('.create-run-modal-header').text('Successfully saved route.')
   $('.create-run').val('')
@@ -39,8 +68,10 @@ const showAllMyRunsFailure = (error) => {
 }
 
 const showOneRunSuccess = (data) => {
-  console.log(data)
   store.runs = data.runs
+  const startLoc = data.run.route[0]
+  console.log('within showOneRunSuccess, modified data is ', (data.run.route[0]))
+  initialize(startLoc)
   // $('#display').empty()
   // const showAllRunsHtml = showAllRunsTemplate({ runs: data.runs })
   // $('#display').append(showAllRunsHtml)
