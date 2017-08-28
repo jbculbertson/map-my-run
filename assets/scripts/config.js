eaa6b95b1e4da12c4cdb56dd2f61d32f1c8a98ed
jbculbertson <jbculbertson@gmail.com>
2017-08-28 10:31:00 -0400

    REmove trailing slash from config url
    - was causing not to hit back end once deployed

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://glacial-oasis-55159.herokuapp.com/',
+    production: 'https://glacial-oasis-55159.herokuapp.com',
     development: 'http://localhost:4741'
   }
 }
